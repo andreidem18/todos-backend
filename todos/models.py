5369591c17fc70d09bae0056051f653105d45f8b
Andres David Mendoza Marquez <andres.david.mm@hotmail.com>
2022-01-12 19:02:09 -0500

    swiched to camel case

--- todos/models.py
+++ todos/models.py
@@ -1,11 +1,9 @@
 from django.db import models
 
-from priorities.models import Priority
-
 class Todo(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
-    is_complete = models.BooleanField()
+    isComplete = models.BooleanField()
 
     def __str__(self):
         return self.title
--- todos/views.py
+++ todos/views.py
@@ -1,15 +1,11 @@
 from django.shortcuts import render
-from rest_framework import status
-from rest_framework.response import Response
-from items.models import Item
-from priorities.models import Priority
 from .models import Todo
 from rest_framework import serializers, viewsets
 
 class TodoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Todo
-        fields = ('id', 'title', 'description', 'is_complete')
+        fields = ('id', 'title', 'description', 'isComplete')
 
 
 class TodoViewSet(viewsets.ModelViewSet):
